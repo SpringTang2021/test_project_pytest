7aa4ce249e55752964edd44b02e1c0149a329d49
SpringTang2021 <my.journey@live.cn>
2022-05-24 18:09:55 +0800

    delete files

--- test_lib/biz_lib.py
+++
@@ -1,6 +0,0 @@
-class Foo:
-    def __init__(self, val):
-        self.val = val
-
-    def __eq__(self, other):
-        return self.val == other.val
--- test_project/group_function_name1/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import logging
-import common_lib
-
-@pytest.fixture(scope="package", autouse=True)
-def group_fixture():
-    logging.getLogger().info("begin run group fixture for  %s"%common_lib.get_group_name(__file__))    
-    return 3
